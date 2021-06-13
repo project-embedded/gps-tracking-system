453555dc4ff71f775aaaf8ab6df6fed3939666ff
nourabdulnasser <85139658+nourabdulnasser@users.noreply.github.com>
2021-06-13 13:43:36 -0700

    Update UART.c

--- UART.c
+++ UART.c
@@ -1,4 +1,9 @@
-void UART7_Init ()
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+#include "Functions.h"
+
+
+void UART7_Init (void)
 {
 
     SYSCTL_RCGCUART_R |= 0x80;
@@ -7,7 +12,7 @@ void UART7_Init ()
     UART7_CTL_R &= ~0x01;
     UART7_IBRD_R = 104; //BR = 9600
     UART7_FBRD_R = 11;
-    UART7_LCRH_R = 0x60; //8-bit length , no parity
+    UART7_LCRH_R = 0x70; //8-bit length , no parity, 1 stop bit, fifo enable
     UART7_CTL_R |= 0x301;
     
     GPIO_PORTE_AFSEL_R |= 0x03;
@@ -16,14 +21,14 @@ void UART7_Init ()
     
 }
 
-uint8_t UART7_Available ()
+uint8_t UART7_Available (void)
 {
     return ((UART7_FR_R & 0x10) == 0x10) ? 0 : 1; //flag values and RxFE bits
 }
 uint8_t UART7_Read (void)
 {
     while (UART7_Available() != 1); //zero means that fifo empty, no data available to read
-    return (uint8_t)(UART7_DR_R & 0xFF);
+    return (char)(UART7_DR_R & 0xFF);
 }
 
 //void UART7_Write (uint8_t data)
