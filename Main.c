f99ccd6fdbff1a46e4a499285a1cbbcec7f6d7ac
Mariam-Nader-Saad <85309131+Mariam-Nader-Saad@users.noreply.github.com>
2021-06-13 23:25:35 +0200

    Update and rename Main to Main.c

--- Main.c
+++ Main.c
@@ -1,7 +1,13 @@
 #include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "tm4c123gh6pm.h"
+#include "Functions.h"
 
 uint8_t i, flag, counter, idx;
+uint16_t dist = 0, on=1;
 char c, Lat[12], Lon[12];
+float x1, y1, x2, y2, latValue, lonValue;
 
 void getLatLon(void){
 	while(1){
@@ -18,23 +24,35 @@ void getLatLon(void){
 	}
 }
 
-uint16_t calcDist (double x1, double y1, double x2, double y2)
+uint16_t calcDist (float x1, float y1, float x2, float y2)
 {
 	return (uint16_t)(sqrt( pow(x1-x2, 2) + pow(y1-y2, 2) ) );
 }
 
-uint16_t dist = 0, on=1;
-double x1=0, y1=0, x2=50, y2=40;
 
 int main()
 {
   PortA_Init ();
   PortB_Init ();	
   PortF_Init ();
-//UART7_Init ();
+  UART0_Init ();
+  UART7_Init ();
+	
+  getLatLon();
+  latValue = parseLatLon(Lat);
+  lonValue = parseLatLon(Lon);
+  x1 = getX (latValue, lonValue);
+  y1 = getY (latValue, lonValue);
 	
   while(ReadSW() != 0x00)
   {
+    flag = 0;
+    getLatLon();
+    latValue = parseLatLon(Lat);
+    lonValue = parseLatLon(Lon);
+    x2 = getX (latValue, lonValue);
+    y2 = getY (latValue, lonValue);
+	  
     dist += calcDist(x1, y1, x2, y2);
     WriteData_B (dist);
     if (dist >= 100)
@@ -46,7 +64,6 @@ int main()
 	    } 
     }
     x1=x2, y1=y2;
-    x2=350, y2=240;
   }
   BLed();
   return 0;
