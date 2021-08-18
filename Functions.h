344b9103a2ae16a7485e6742f36e2c0bd399adc5
Maria-Osama1809094 <85039110+Maria-Osama1809094@users.noreply.github.com>
2021-08-18 16:26:47 +0200

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -1,6 +1,20 @@
 
-#include "PortA.h"
-#include "PortB.h"
-#include "PortF.h"
-#include "UART.h"
-#include "Parse.h"
+void PortA_Init (void);
+void WriteData_A (uint16_t distance);
+
+void PortB_Init (void);
+void WriteData_B (uint16_t distance);
+
+void PortF_Init (void);
+void RLed (void); 
+void BLed (void);
+uint8_t ReadSW(void);
+
+void UART0_Init (void);
+void UART7_Init (void);
+uint8_t UART7_Available (void);
+char UART7_Read (void);
+
+float parseLatLon (char * gps);
+float getX (float latf, float lonf);
+float getY (float latf, float lonf);
