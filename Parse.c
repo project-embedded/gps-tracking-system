e9d2451e73ae6249bde062ad4da1cca746866f63
Maria-Osama1809094 <85039110+Maria-Osama1809094@users.noreply.github.com>
2021-08-18 16:13:48 +0200

    Update Parse.c

--- Parse.c
+++ Parse.c
@@ -1,30 +1,17 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "tm4c123gh6pm.h"
-#include "Functions.h"
 #include <math.h>
 #define PI 3.14159265359
 
-uint8_t digitCount;
 uint16_t I;
 float X, Y, num, F, P;
 
-float parseLatLon (char * gps)
-{
+float parseLatLon (char * gps){
 	P = 0;
-	digitCount = ( gps[4] == '.' ? 2 : 3); 
 	num = atof(gps);
-	
-  if ( digitCount == 2 ){
 	I = num / 100;
-  F = num - I*100;
-	}
-	 
-  else{
-	I = num / 100;
-  F = num - I*100;
-	}
-	
+	F = num - I*100;
 	P = I + F/ 60;
 	return P; 
 }
